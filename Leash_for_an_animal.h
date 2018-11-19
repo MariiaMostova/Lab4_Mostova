63fc0dee278461298412a74281078b7e5a5d4d7c
Mariia Mostova <were3437@gmail.com>
2018-11-19 19:27:09 +0200

    Лаба 4: опис класу Повідок для тварини, котрий містить 4 приватні, 2 публічні, 2 захищені поля, 6 методів, 2 конструктори і 1 деструктор

---
+++ Leash_for_an_animal.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+class Leash_for_an_animal
+{
+private:
+	string manufactorer;
+	int max_length;
+	int max_masa;
+	string color;
+public:
+	string getManufactorer();
+	int getMax_length();
+	int getMax_masa();
+	string getColor();
+	string getPetName();
+	string getMatterial();
+	double diametr;
+	string pet;
+	Leash_for_an_animal() {
+		manufactorer = "Mary";
+		max_length=4;
+		max_masa = 15; 
+		color="red";
+		diametr = 15.2;
+		pet="dog";
+		petName = "Jony";
+		matterial = "leather";
+	};
+	Leash_for_an_animal(string manufactorer, int max_length, int max_masa, string color, double diametr, string petName, string pet, string matterial);
+	~Leash_for_an_animal();
+protected:
+	string petName;
+	string matterial;
+};
